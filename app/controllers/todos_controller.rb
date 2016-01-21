13151af9baeb457e3adfc0907f9a5d86754324ec
Bryan Garza <bgarza1016@gmail.com>
2016-01-20 18:32:06 -0800

    add create to_do

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,17 @@ class TodosController < ApplicationController
 	def new
 		@todo = Todo.new
 	end
+
+	def create 
+		@todo = Todo.create(todo_params)
+		redirect_to todos_path
+	end
+
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+
+	private
+	def todo_params
+		params.require(:todo).permit(:item) end
 end
